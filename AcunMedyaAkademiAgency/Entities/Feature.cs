237085822a9a085db7962fa74086e3c2ebf99d2f
JastJoper06 <sealteam0156@hotmail.com>
2025-03-03 23:55:02 +0300

    Add project files.

---
+++ AcunMedyaAkademiAgency/Entities/Feature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcunMedyaAkademiAgency.Entities
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
---
+++ AcunMedyaAkademiAgency/Migrations/202503031129207_mig6.cs
@@ -0,0 +1,52 @@
+﻿namespace AcunMedyaAkademiAgency.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig6 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clients",
+                c => new
+                    {
+                        ClientId = c.Int(nullable: false, identity: true),
+                        icon = c.String(),
+                        Url = c.String(),
+                    })
+                .PrimaryKey(t => t.ClientId);
+            
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        ContactId = c.Int(nullable: false, identity: true),
+                        NameSurname = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        TelNo = c.String(nullable: false),
+                        Message = c.String(nullable: false),
+                        CreatedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ContactId);
+            
+            CreateTable(
+                "dbo.SocialMedias",
+                c => new
+                    {
+                        SocialMediaId = c.Int(nullable: false, identity: true),
+                        icon = c.String(),
+                        Url = c.String(),
+                    })
+                .PrimaryKey(t => t.SocialMediaId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.SocialMedias");
+            DropTable("dbo.Contacts");
+            DropTable("dbo.Clients");
+        }
+    }
+}
